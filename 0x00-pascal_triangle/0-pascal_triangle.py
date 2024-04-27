ad3919bffe3e7d5c0d2061af549fe54c5dd5a9d5
philip Opiyo <opiyophilipo@gmail.com>
2024-04-27 15:38:43 +0300

    Refactor pascal_triangle function in 0x00-pascal_triangle.py for readability

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """Pascal's Triangle"""
 
+
 def pascal_triangle(n):
-    """Returns a list of lists of integers representing the Pascal's triangle of n"""
+    """
+    Returns a list of lists of integers
+    representing the Pascal's triangle of n
+    """
 
     if n <= 0:
         return []
-    
+
     # Initialize the Pascal's triangle as a list of lists
     triangle = []
 
@@ -15,7 +19,7 @@ def pascal_triangle(n):
         if triangle:
             prev_row = triangle[-1]
             for j in range(1, len(prev_row)):
-                row.append(prev_row[j-1] + prev_row[j])
+                row.append(prev_row[j - 1] + prev_row[j])
             row.append(1)
         triangle.append(row)
 
