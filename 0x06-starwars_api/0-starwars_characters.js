eefb6864fa66f01df9557eacbba85d5ccdfca359
philip Opiyo <opiyophilipo@gmail.com>
2024-06-05 03:20:33 -0700

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,43 +1,16 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-// Check if the movie ID is provided as a command-line argument
-if (process.argv.length < 3) {
-  console.log('Usage: node starwars.js <Movie ID>');
-  process.exit(1);
-}
-
-const movieId = process.argv[2];
-
-// URL to get the movie details
-const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-// Function to fetch data from a given URL
-const fetchData = (url, callback) => {
-  request(url, (error, response, body) => {
-    if (error) {
-      console.error('Error fetching data:', error);
-      return;
-    }
-
-    if (response.statusCode !== 200) {
-      console.error('Failed to fetch data:', response.statusCode);
-      return;
-    }
-
-    callback(JSON.parse(body));
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) throw error;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (error, response, body) {
+    if (error) throw error;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
   });
 };
-
-// Fetch the movie data
-fetchData(movieUrl, (movieData) => {
-  const characters = movieData.characters;
-
-  // Fetch and print each character's name
-  characters.forEach((characterUrl) => {
-    fetchData(characterUrl, (characterData) => {
-      console.log(characterData.name);
-    });
-  });
-});
